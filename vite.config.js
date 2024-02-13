bc86fde70b76d8babd88f43bebaa8a574b2de947
AthenaKiriakoulis <athena.kiriakoulis@gmail.com>
2024-02-13 18:54:59 -0500

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ 
+  base: "https://github.com/AthenaKiriakoulis/portfolio-website.git", 
+  plugins: [react()], }) 
